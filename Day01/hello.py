70d94514cc8e85083fee10f9f7b5925e54606d1c
leitianci <2045915235@qq.com>
2024-02-26 15:25:54 +0800

    falg hello commit

---
+++ Day01/hello.py
@@ -0,0 +1,13 @@
+# 开发者 ： 雷天慈
+# 开发时间 ： 2024/2/26 15:18
+
+"""
+第一个Python程序 - hello, world!
+向伟大的Dennis M. Ritchie先生致敬
+"""
+
+print('hello, world!')
+# print("你好,世界！")
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
