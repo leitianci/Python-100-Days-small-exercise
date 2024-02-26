c781022ed2066d8eecc05cc33e52dee7a3af16bf
leitianci <2045915235@qq.com>
2024-02-26 14:47:40 +0800

    exercise commit

---
+++ Day01/exercise02.py
@@ -0,0 +1,17 @@
+# 开发者 ： 雷天慈
+# 开发时间 ： 2024/2/26 14:46
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
